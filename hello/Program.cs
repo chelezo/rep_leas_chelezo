203e4efa81d26034ad3cb855e19b249325c9bce4
chelezo <alex.chelezo@gmail.com>
2021-12-24 15:04:59 +0200

    hello reverse

---
+++ hello/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] ArrayHello = new char[] { 'H', 'e', 'l', 'l', 'o' };
+            for (int i = ArrayHello.Length - 1; i >= 0; i--)
+                Console.Write(ArrayHello[i]);
+        }
+    }
+}
