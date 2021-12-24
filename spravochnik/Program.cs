3ddc11a81f4d06458d83828738c36707a7032fac
chelezo <alex.chelezo@gmail.com>
2021-12-24 13:40:40 +0200

    справочник рабочий!

--- spravochnik/Program.cs
+++ spravochnik/Program.cs
@@ -8,48 +8,56 @@ namespace spravochnik
         {
             string[,] arraySpravochnik = new string [,]
             {
-               { "Петров Александр","Телефон 098543245",},
-               { "Иванов Петр","Телефон 0943568903" },
-               { "Киркоров Андрей", "Email ololo@gmail.com" },
-               { "Литвиненко Алексей", "Телефон 0847532218"},
-               { "Андриенко Максим"," email lobzik@gmail.com" }
+               { "Петров","Телефон 098543245",},
+               { "Иванов","Телефон 0943568903" },
+               { "Киркоров", "Email ololo@gmail.com" },
+               { "Литвиненко", "Телефон 0847532218"},
+               { "Андриенко"," email lobzik@gmail.com" }
 
             };
-            //Console.WriteLine(arraySpravochnik.Length);
-            arraySpravochnik[0,0] = "Фамилия";
-            arraySpravochnik[0,1] = "Телефон или Емейл";
+           // Console.WriteLine(arraySpravochnik[0,1]);
+            //arraySpravochnik[0,0] = "Фамилия";
+            //arraySpravochnik[0,1] = "Телефон или Емейл";
             Console.WriteLine("Введите фамилию (Петров Иванов Киркоров)");
             string enterFamily = Console.ReadLine();
             bool Found = false;
 
-            for (int i=0; i < arraySpravochnik.Length; i++)
+            for (int i = 0; i < arraySpravochnik.Length; i++)
                 
             {
-                //int Spravochnik = (i++);
-
-                if (arraySpravochnik[i,0] == enterFamily)
-                {
-                    Console.WriteLine($" Житель  {arraySpravochnik[i,0]} найден ");
-                  //  switch (i)
-                  //  {
-                  //      case 0:
-                  //          Console.WriteLine("Номер телефона 096553221 Емейл ree@gmail.com");
-                  //          break;
-                  //      case 1:
-                  //          Console.WriteLine("Номер телефона 053213228 Емейл buu@gmail.com");
-                  //          break;
-                  //      case 2:
-                  //          Console.WriteLine("Номер телефона 053563333 Емейл ololo@gmail.com");
-                  //          break;
-                  //  }
-                    
-                        
-                        Found = true;
-                                      
-                    
-                }
-                
                
+                      
+                    //int Spravochnik = (i++);
+
+                    if (arraySpravochnik[i, 0] == enterFamily)
+                    {
+                        Console.WriteLine($" Житель  {arraySpravochnik[i,0]} найден ");
+                         switch (i)
+                         {
+                              case 0:
+                                  Console.WriteLine(arraySpravochnik[0, 1]);
+                                  break;
+                              case 1:
+                            Console.WriteLine(arraySpravochnik[1, 1]);
+                            break;
+                             case 2:
+                            Console.WriteLine(arraySpravochnik[2, 1]);
+                            break;
+                        case 3:
+                            Console.WriteLine(arraySpravochnik[3, 1]);
+                            break;
+                        case 4:
+                            Console.WriteLine(arraySpravochnik[4, 1]);
+                           break;
+                    }
+
+
+                        Found = true;
+                    //Console.WriteLine($" Его {i} ");
+
+
+
+                }
             }
             if (!Found)
             {
