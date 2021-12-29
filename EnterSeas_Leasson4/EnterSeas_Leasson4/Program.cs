8a45a7234c0ed43dd0d87a0de6dd2dce37d45a3c
chelezo <alex.chelezo@gmail.com>
2021-12-29 23:19:34 +0200

    Leas_4_season

--- EnterSeas_Leasson4/EnterSeas_Leasson4/Program.cs
+++ EnterSeas_Leasson4/EnterSeas_Leasson4/Program.cs
@@ -6,50 +6,50 @@ namespace EnterSeas_Leasson4
     {
         [Flags]
         enum Months
-         {
-             January = 1,
-             February = 2,
-             March = 3,
-             April = 4,
-             May = 5,
-             June = 6,
-             July = 7,
-             August = 8,
-             September = 9,
-             October = 10,
-             November = 11,
-             December = 12,
-         }
+        {
+            January = 1,
+            February = 2,
+            March = 3,
+            April = 4,
+            May = 5,
+            June = 6,
+            July = 7,
+            August = 8,
+            September = 9,
+            October = 10,
+            November = 11,
+            December = 12,
+        }
 
 
         [Flags]
-        enum Seasons 
+        enum Seasons
         {
-            Winter = Months.December | Months.January | Months.February,
-            Spring = Months.March | Months.April | Months.May,
-            Summer = Months.June | Months.July | Months.August,
-            Autumn = Months.September | Months.October | Months.November
+            Winter ,
+            Spring ,
+            Summer ,
+            Autumn 
         }
         static void Main(string[] args)
-            
+
         {
             //System.Console.WriteLine(Months.Winter);
             int MonthInt;
-            //Seasons SeasonsRead = Seasons.Winter | Seasons.Spring | Seasons.Autumn;
-          // System.Console.ReadLine();
+            Seasons SeasonsRead = Seasons.Winter | Seasons.Spring | Seasons.Autumn;
             MonthInt = Convert.ToInt32(System.Console.ReadLine());
-            if () 
-            { }
-           // if (MonthInt == 12)
-           //         {
-           //     System.Console.WriteLine($"Месяц {(Months)MonthInt} время года {Seasons.Winter}");
-           // }
-            // string MonthInt = System.Console.ReadLine();
-            // System.Console.WriteLine((Seasons)MonthInt);
+            var SeasConsole = (Months)MonthInt;
+            //System.Console.WriteLine(SeasConsole);
 
-
-
-
+            if (MonthInt < 3 || MonthInt == 12)
+                Console.WriteLine($"{Seasons.Winter}, {SeasConsole} ");
+            else if (MonthInt < 6)
+                Console.WriteLine($"{Seasons.Spring}, {SeasConsole} ");
+            else if (MonthInt < 9)
+                Console.WriteLine($"{ Seasons.Summer}, {SeasConsole} ");
+            else if (MonthInt < 12)
+                Console.WriteLine($"{Seasons.Autumn}, {SeasConsole} ");
+            else
+                Console.WriteLine("Шота вы не то тыцнули");
         }
     }
 }
